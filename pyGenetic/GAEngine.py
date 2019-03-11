1695237ec777b8651f5b0b5b21899f644070acf4
Bharatraj Telkar <telkarraj@gmail.com>
2019-03-11 16:49:36 +0530

    Added 4 Crossover methods and 1 Mutation method

--- pyGenetic/GAEngine.py
+++ pyGenetic/GAEngine.py
@@ -143,10 +143,10 @@ if __name__ == '__main__':
 	matrix = [[0,172,145,607,329,72,312,120],[172,0,192,494,209,158,216,92],[145,192,0,490,237,75,205,100],[607,494,490,0,286,545,296,489],[329,209,237,286,0,421,49,208],[72,158,75,545,421,0,249,75],[312,216,205,296,49,249,9,194],[120,92,100,489,208,75,194,0]]
 	# best sequence i found: 0 5 2 7 1 6 4 3
 
-	ga = GAEngine(8,factory,100,fitness_type='min',mut_prob = 0.5)
-	ga.addCrossoverHandler(Utils.CrossoverHandlers.distinct, 9)
+	ga = GAEngine(8,factory,100,fitness_type='min',mut_prob = 0.3)
+	ga.addCrossoverHandler(Utils.CrossoverHandlers.PMX, 9)
 	ga.addCrossoverHandler(Utils.CrossoverHandlers.distinct, 4)
-	ga.addCrossoverHandler(Utils.CrossoverHandlers.distinct, 3)
+	ga.addCrossoverHandler(Utils.CrossoverHandlers.OX, 3)
 	ga.addMutationHandler(Utils.MutationHandlers.swap)
 	ga.setSelectionHandler(Utils.SelectionHandlers.SUS)
 	ga.setFitnessHandler(Utils.Fitness.TSP, matrix)
--- pyGenetic/Utils.py
+++ pyGenetic/Utils.py
@@ -79,6 +79,13 @@ class MutationHandlers:
 		newchrom[index], newchrom[index+1] = newchrom[index+1], newchrom[index]
 		return newchrom
 
+	# only when genes are binary
+	@staticmethod
+	def bitFlip(chromosome):
+		index = random.randint(0,len(chromosome))
+		chromosome[index] = type(chromosome[index])(not chromosome[index])
+		return chromosome
+
 class CrossoverHandlers:
 	@staticmethod
 	def distinct(chromosome1,chromosome2):
@@ -93,6 +100,74 @@ class CrossoverHandlers:
 			if i not in new_chromosome2:
 				new_chromosome2.append(i)
 		return new_chromosome1,new_chromosome2
+		
+	@staticmethod
+	def onePoint(chromosome1, chromosome2):
+		size = min(len(chromosome1), len(chromosome2))
+		r = random.randint(1, size - 1)
+		new_chromosome1 = chromosome1[:r]+chromosome2[r:]
+		new_chromosome2 = chromosome2[:r]+chromosome1[r:]
+		return new_chromosome1, new_chromosome2
+
+	@staticmethod
+	def twoPoint(chromosome1, chromosome2):
+		size = min(len(chromosome1), len(chromosome2))
+		a = random.randint(1, size - 2)
+		b = random.randint(1, size - 1)
+		if b==a:
+			b+=1
+		elif b<a:
+			a,b=b,a
+		new_chromosome1 = chromosome1[:a]+chromosome2[a:b]+chromosome1[b:]
+		new_chromosome2 = chromosome2[:a]+chromosome1[a:b]+chromosome2[b:]
+		return new_chromosome1, new_chromosome2
+		
+	@staticmethod
+	def PMX(chromosome1, chromosome2): # Partially Matched Crossover
+		size = min(len(chromosome1), len(chromosome2))
+		a = random.randint(1, size - 2)
+		b = random.randint(1, size - 1)
+		if b==a:
+			b+=1
+		elif b<a:
+			a,b=b,a
+		new_chromosome1 = chromosome1[:a]+chromosome2[a:b]+chromosome1[b:]
+		new_chromosome2 = chromosome2[:a]+chromosome1[a:b]+chromosome2[b:]
+		mapping1=chromosome2[a:b]
+		mapping2=chromosome1[a:b]
+		i = b
+		while(i!=a):
+			while new_chromosome1[i] in mapping1:
+				new_chromosome1[i] = mapping2[mapping1.index(new_chromosome1[i])]
+			while new_chromosome2[i] in mapping2:
+				new_chromosome2[i] = mapping1[mapping2.index(new_chromosome2[i])]
+			i = (i+1)%size
+		return new_chromosome1, new_chromosome2
+	
+	@staticmethod
+	def OX(chromosome1, chromosome2): # Ordered Crossover
+		size = min(len(chromosome1), len(chromosome2))
+		a = random.randint(1, size - 2)
+		b = random.randint(1, size - 1)
+		if b==a:
+			b+=1
+		elif b<a:
+			a,b=b,a
+		new_chromosome1 = [None]*(a)+chromosome1[a:b]+[None]*(size-b)
+		new_chromosome2 = [None]*(a)+chromosome2[a:b]+[None]*(size-b)
+		i,j,k = b,b,b
+		while True:
+			if chromosome2[i] not in new_chromosome1:
+				new_chromosome1[j] = chromosome2[i]
+				j = (j+1)%size
+			if chromosome1[i] not in new_chromosome2:
+				new_chromosome2[k] = chromosome1[i]
+				k = (k+1)%size
+			i = (i+1)%size
+			if i==b or (j==a and k==a):
+				break
+		return new_chromosome1, new_chromosome2
+
 
 class Fitness:
 	@staticmethod
