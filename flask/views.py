784b995ece83ca8e7b5038ef311191f5ecea87b8
Vivek Patil, Shreyas <Shreyas.Patil@philips.com>
2019-03-11 13:19:56 +0530

    Added fields in UI

--- flask/views.py
+++ flask/views.py
@@ -26,10 +26,10 @@ def ga_online():
 def commonCodeCreate():
 	payload = request.get_json()
 	session["fileType"] = ".tar.gz"
-	
+
 	if(len(payload["fileType"])<=8): # else, use what user chose
 		session["fileType"] = payload["fileType"]
-	
+
 	del payload["fileType"]
 
 	print("Payload Data: ", payload)
@@ -39,7 +39,7 @@ def commonCodeCreate():
 		session["pattern"] = "adapter"
 		s = render.Adapter(json.loads(json.dumps(payload)))
 		s.render()
-	
+
 	elif(payload["pattern"] == "state"):
 		session["pattern"] = "state"
 		s = render.State(json.loads(json.dumps(payload)))
@@ -49,12 +49,12 @@ def commonCodeCreate():
 		session["pattern"] = "iterator"
 		s = render.Iterator(json.loads(json.dumps(payload)))
 		s.render()
-    
+
 	elif(payload["pattern"] == "policy"):
 		session["pattern"] = "policy"
 		s = render.Policy(json.loads(json.dumps(payload)))
 		s.render()
-	
+
 	print("Session Data: ", session)
 
 	return jsonify({
@@ -65,30 +65,10 @@ def commonCodeCreate():
 @app.route("/downloadCode",methods=["POST"])
 def downloadCode():
 	return redirect(url_for("codeDownload",
-						filename=session.get("pattern") + session.get("fileType"), 
-						patternType=session.get("pattern"), 
+						filename=session.get("pattern") + session.get("fileType"),
+						patternType=session.get("pattern"),
 						fileType=session.get("fileType")))
 
-
-@app.route("/codeDownload/<path:filename>/<path:patternType>/<path:fileType>")
-def codeDownload(filename,patternType,fileType):
-	if(fileType in app.config["ALLOWED_COMPRESSED_FILE_EXTENSIONS"]):
-		beautify_cmd = "astyle --style=attach -s4 -xG -xe -YFpCHUSK --recursive ./GOF_templates/templates/output/" + patternType + "/*.cpp,*.h"
-		os.system(beautify_cmd)
-		os.system("find . -type f -name '*.orig' -exec rm {} +")
-		
-		utils.checkAndCreateDownloadsFolder()
-
-		utils.makeCompressedfile(outputFilename=os.path.join(app.config['CODE_DOWNLOAD_FOLDER'],filename),
-						   sourceDir=os.path.join("./GOF_templates/templates/output/",patternType),
-						   fileType=fileType)
-
-		print("Path:",os.path.join(app.config["USER_DOWNLOAD_FOLDER"],filename))
-
-		return send_from_directory(app.config["USER_DOWNLOAD_FOLDER"],filename,as_attachment=True)
-	else:
-		print("codeDownload error")
-		return (render_template("index.html",isError=True,errorMessage="Please create code by choosing a pattern first."),302)
 
 
 app.run()
