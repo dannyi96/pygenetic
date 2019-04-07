2b2c094f1aa19e06e79c250d046ed0eff90957fe
Daniel Isaac <danielbcbs2@gmail.com>
2019-04-07 10:39:52 +0530

    small bug fix

--- flask/static/js/State/state.js
+++ flask/static/js/State/state.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
         }
         else if(generationNumber == 1)
         {
+                $('#results').empty();
                 $.ajax(
                     {
                         type: "POST",
