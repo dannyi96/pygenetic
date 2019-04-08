9c595ec09b8bb0aa017155bb3a220042c84cc3c1
Daniel Isaac <danielbcbs2@gmail.com>
2019-04-08 20:10:56 +0530

    Issue #43 taken care of

--- flask/static/js/State/state.js
+++ flask/static/js/State/state.js
@@ -10,6 +10,10 @@ $(document).ready(function() {
         console.log(generationNumber);
         if(generationNumber > MAX_ITER)
         {
+            console.log('HERE');
+            var date = new Date();
+            var timestamp = date.getTime();
+            document.getElementById('fitness_graph_image').src = '/plot_fitness_graph?lastmod='+ timestamp;
             return;
         }
         else if(generationNumber == 1)
