8c3ea767dfd93aa8e8e5e5db992a3c492a75917e
Daniel Isaac <danielbcbs2@gmail.com>
2019-04-11 00:26:33 +0530

    Issue #47: mutation,crossover

--- pyGenetic/ANNEvolve.py
+++ pyGenetic/ANNEvolve.py
@@ -5,6 +5,7 @@ from keras.layers import Dense
 import numpy
 import random
 import Utils
+import copy
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load pima indians dataset
@@ -21,7 +22,6 @@ class ANNChromosomeFactory(ChromosomeFactory):
 
 	def createChromosome(self):
 		return [random.choice(self.neuronsPerLayer), random.choice(self.activations), random.choice(self.neuronsPerLayer), random.choice(self.activations), random.choice(self.optimizers)]
-
 
 class ANNEvolve:
 	def __init__(self,X,Y,neuronsPerLayer=[2,5,10,12],activations=['relu','sigmoid'],optimizers=['adam'],loss='binary_crossentropy',metrics='accuracy'):
@@ -41,7 +41,9 @@ class ANNEvolve:
 		ga = GAEngine.GAEngine(self.factory,population_size=10,fitness_type='min')
 		ga.setFitnessHandler(ANNEvolve.fitness,self.input_dim,self.loss,self.metrics)
 		ga.setSelectionHandler(Utils.SelectionHandlers.best)
-		ga.evolve(1)
+		ga.addCrossoverHandler(Utils.CrossoverHandlers.onePoint,1)
+		ga.addMutationHandler(ANNEvolve.mutation,1,self.neuronsPerLayer,self.activations,self.optimizers)
+		ga.evolve(2)
 
 	@staticmethod
 	def fitness(chromosome,input_dim,loss,metrics):
@@ -53,13 +55,26 @@ class ANNEvolve:
 		# Compile model
 		model.compile(loss=loss, optimizer=chromosome[4], metrics=[metrics])
 		# Fit the model
-		model.fit(X, Y, epochs=30, batch_size=10)
+		model.fit(X, Y, epochs=30, batch_size=10,verbose=0)
 		# evaluate the model
 		scores = model.evaluate(X, Y)
 		print(scores)
 		print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 		print('Loss', scores[0])
 		return scores[0]
+
+	@staticmethod
+	def mutation(chromosome,neuronsPerLayer,activations,optimizers):
+		r = random.randint(0,4)
+		newchrom = copy.copy(chromosome)
+		if r == 0 or r == 2:
+			newchrom[r] = random.choice(neuronsPerLayer)
+		elif r == 1 or r == 3:
+			newchrom[r] = random.choice(activations)
+		else:
+			newchrom[r] = random.choice(optimizers)
+		return newchrom
+
 
 if __name__ == '__main__':
 	a = ANNEvolve(X,Y)
