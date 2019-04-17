b9e36b4fa57cea6d18636693720fdfdcaf593914
Daniel Isaac <danielbcbs2@gmail.com>
2019-04-17 21:57:16 +0530

    Issue #47: ANN topology completed

--- pyGenetic/ANNEvolve.py
+++ pyGenetic/ANNEvolve.py
@@ -6,56 +6,62 @@ import numpy
 import random
 import Utils
 import copy
-# fix random seed for reproducibility
-numpy.random.seed(7)
-# load pima indians dataset
-dataset = numpy.loadtxt("input.csv", delimiter=",")
-# split into input (X) and output (Y) variables
-X = dataset[:,0:8]
-Y = dataset[:,8]
 
-class ANNChromosomeFactory(ChromosomeFactory):
-	def __init__(self,neuronsPerLayer,activations,optimizers):
+class ANNTopologyChromosomeFactory(ChromosomeFactory):
+	def __init__(self,hiddenLayers,neuronsPerLayer,activations,optimizers):
 		self.neuronsPerLayer = neuronsPerLayer
 		self.activations = activations
 		self.optimizers = optimizers
+		self.hiddenLayers = hiddenLayers
 
 	def createChromosome(self):
-		return [random.choice(self.neuronsPerLayer), random.choice(self.activations), random.choice(self.neuronsPerLayer), random.choice(self.activations), random.choice(self.optimizers)]
+		chromosome = []
+		for i in range(self.hiddenLayers):
+			chromosome.extend([random.choice(self.neuronsPerLayer), random.choice(self.activations)])
+		chromosome.append(random.choice(self.activations))
+		chromosome.append(random.choice(self.optimizers))
+		return chromosome
 
-class ANNEvolve:
-	def __init__(self,X,Y,neuronsPerLayer=[2,5,10,12],activations=['relu','sigmoid'],optimizers=['adam'],loss='binary_crossentropy',metrics='accuracy'):
+class ANNTopologyEvolve:
+	def __init__(self,X,Y,hiddenLayers,population_size=10,neuronsPerLayer=[2,5,10,12],activations=['relu','sigmoid'],optimizers=['adam'],loss='binary_crossentropy',metrics='accuracy',epochs=30,batch_size=10):
 		self.X = X
 		self.Y = Y
+		if hiddenLayers < 1:
+			raise Exception('No of hiddenLayers should be greater than zero')
+		self.hiddenLayers = hiddenLayers
 		self.input_dim = len(X[0])
 		self.neuronsPerLayer = neuronsPerLayer
 		self.activations = activations
 		self.optimizers = optimizers
 		self.loss = loss
 		self.metrics = metrics
-		self.factory = ANNChromosomeFactory(neuronsPerLayer,activations,optimizers)
+		self.epochs = epochs
+		self.batch_size = batch_size
+		self.population_size = population_size
+		self.factory = ANNTopologyChromosomeFactory(hiddenLayers,neuronsPerLayer,activations,optimizers)
 		print(self.factory.createChromosome())
 		#self.fitness(self.factory.createChromosome(),self.input_dim,self.loss,self.metrics)
 
-	def new_evolve(self):
-		ga = GAEngine.GAEngine(self.factory,population_size=10,fitness_type='min')
-		ga.setFitnessHandler(ANNEvolve.fitness,self.input_dim,self.loss,self.metrics)
+	def evolve(self,noOfGenerations=100):
+		ga = GAEngine.GAEngine(self.factory,population_size=self.population_size,fitness_type='min')
+		ga.setFitnessHandler(ANNTopologyEvolve.fitness,self.X,self.Y,self.hiddenLayers,self.input_dim,self.loss,self.metrics,self.epochs,self.batch_size)
 		ga.setSelectionHandler(Utils.SelectionHandlers.best)
 		ga.addCrossoverHandler(Utils.CrossoverHandlers.onePoint,1)
-		ga.addMutationHandler(ANNEvolve.mutation,1,self.neuronsPerLayer,self.activations,self.optimizers)
-		ga.evolve(2)
+		ga.addMutationHandler(ANNTopologyEvolve.mutation,1,self.neuronsPerLayer,self.activations,self.optimizers)
+		ga.evolve(noOfGenerations)
 
 	@staticmethod
-	def fitness(chromosome,input_dim,loss,metrics):
+	def fitness(chromosome,X,Y,hidden_layers,input_dim,loss,metrics,epochs,batch_size):
 		# create model
 		model = Sequential()
 		model.add(Dense(chromosome[0], input_dim= input_dim, activation=chromosome[1]))
-		model.add(Dense(chromosome[2], activation=chromosome[3]))
-		model.add(Dense(1, activation='sigmoid'))
+		for i in range(hidden_layers-1):
+			model.add(Dense(chromosome[i+2], activation=chromosome[i+3]))
+		model.add(Dense(1, activation=chromosome[len(chromosome)-2]))
 		# Compile model
-		model.compile(loss=loss, optimizer=chromosome[4], metrics=[metrics])
+		model.compile(loss=loss, optimizer=chromosome[len(chromosome)-1], metrics=[metrics])
 		# Fit the model
-		model.fit(X, Y, epochs=30, batch_size=10,verbose=0)
+		model.fit(X, Y, epochs=epochs, batch_size=batch_size,verbose=1)
 		# evaluate the model
 		scores = model.evaluate(X, Y)
 		print(scores)
@@ -65,18 +71,19 @@ class ANNEvolve:
 
 	@staticmethod
 	def mutation(chromosome,neuronsPerLayer,activations,optimizers):
-		r = random.randint(0,4)
+		r = random.randint(0,len(chromosome)-1)
 		newchrom = copy.copy(chromosome)
-		if r == 0 or r == 2:
-			newchrom[r] = random.choice(neuronsPerLayer)
-		elif r == 1 or r == 3:
-			newchrom[r] = random.choice(activations)
-		else:
+		if r == len(chromosome) - 1:
 			newchrom[r] = random.choice(optimizers)
+		elif r%2 == 1 or r == len(chromosome)-2:
+			newchrom[r] = random.choice(activations)
+		elif r%2 == 0:
+			newchrom[r] = random.choice(neuronsPerLayer)
 		return newchrom
 
 
 if __name__ == '__main__':
-	a = ANNEvolve(X,Y)
-	a.new_evolve()
+	a = ANNTopologyEvolve(X,Y,hiddenLayers=2)
+	a.evolve()
+	print(a.best_fitness)
 	#a.evolve()
