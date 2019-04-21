986baebf60ff995fb354ee49315b1d0b6dbd52ff
Daniel Isaac <danielbcbs2@gmail.com>
2019-04-21 17:41:38 +0530

    population control test

--- tests/modules/test_GAEngine.py
+++ tests/modules/test_GAEngine.py
@@ -7,6 +7,34 @@ sys.path.append('.')
 
 import GAEngine,Utils,ChromosomeFactory
 
+def test_population_control():
+    # Verify when population control is True
+    factory = ChromosomeFactory.ChromosomeRangeFactory(noOfGenes=8,minValue=1,maxValue=100)
+    ga = GAEngine.GAEngine(factory,population_size=100,fitness_type='max',mut_prob = 0.3,population_control = True)
+    ga.addCrossoverHandler(Utils.CrossoverHandlers.distinct, 4)
+    ga.addMutationHandler(Utils.MutationHandlers.swap)
+    ga.setSelectionHandler(Utils.SelectionHandlers.best)
+    ga.setFitnessHandler(lambda x:sum(x))
+    ga.evolve(1)
+    assert len(ga.population.members) == 100
+
+    # Verify when population control is False
+    def select_all(fitness_mappings, ga):
+        return [i[0] for i in fitness_mappings]
+
+    factory = ChromosomeFactory.ChromosomeRangeFactory(noOfGenes=8,minValue=1,maxValue=100)
+    ga = GAEngine.GAEngine(factory,population_size=100,fitness_type='max',mut_prob = 0.3,population_control = False)
+    ga.addCrossoverHandler(Utils.CrossoverHandlers.distinct, 4)
+    ga.addMutationHandler(Utils.MutationHandlers.swap)
+    ga.setSelectionHandler(select_all)
+    ga.setFitnessHandler(lambda x:sum(x))
+    ga.evolve(1)
+    assert len(ga.population.members) > 100
+
+
+
+
+
 
 #Getters and Setters need not be tested
 #https://stackoverflow.com/questions/6197370/should-unit-tests-be-written-for-getter-and-setters
