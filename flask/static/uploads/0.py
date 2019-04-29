2a99e5fa45b124b161989af89bd837a91e8056a5
Daniel Isaac <danielbcbs2@gmail.com>
2019-04-29 23:36:03 +0530

    Flask app style change

--- flask/static/uploads/0.py
+++ flask/static/uploads/0.py
@@ -1,10 +1,10 @@
 import GAEngine
 import Utils
 import ChromosomeFactory
-factory = ChromosomeFactory.ChromosomeRangeFactory(data_type=int,noOfGenes=8,minValue=0,maxValue=20,duplicates=False)
-ga = GAEngine.GAEngine(factory=factory,population_size=20,cross_prob=0.4,mut_prob=0.2,fitness_type='max',adaptive_mutation=True,use_pyspark=False)
-ga.addCrossoverHandler(Utils.CrossoverHandlers.onePoint,1)
-ga.addMutationHandler(Utils.MutationHandlers.swap,4)
-ga.setSelectionHandler(Utils.SelectionHandlers.basic)
+factory = ChromosomeFactory.ChromosomeRangeFactory(noOfGenes=10,minValue=100,maxValue=200,duplicates=True)
+ga = GAEngine.GAEngine(factory=factory,population_size=100,cross_prob=0.3,mut_prob=0.3,fitness_type='max',adaptive_mutation=True,use_pyspark=False)
+ga.addCrossoverHandler(Utils.CrossoverHandlers.distinct,3)
+ga.addMutationHandler(Utils.MutationHandlers.swap,3)
+ga.setSelectionHandler(Utils.SelectionHandlers.best)
 ga.setFitnessHandler(Utils.Fitness.addition)
-ga.evolve(10)
+ga.evolve(20)
