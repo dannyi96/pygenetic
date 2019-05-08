361d33c65802c3b3a043dfda44fdc6ca6022e4d3
Daniel Isaac <danielbcbs2@gmail.com>
2019-05-08 22:12:11 +0530

    final changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pygenetic",
-    version="0.9.3",
+    version="1.0.0",
     author="Bharatraj S Telkar, Daniel Isaac, Shreyas V Patil",
     author_email="telkarraj@gmail.com, danielbcbs2@gmail.com, pshreyasv100@gmail.com",
     description="An Efficient Python Genetic Algorithm API",
@@ -22,12 +22,7 @@ setuptools.setup(
     ),
     install_requires=[
           'rstr==2.2.6',
-          #'Keras==2.2.4',
           'numpy==1.15.4',
-          #'pyspark==2.4.1',
-          #'pytest==4.0.0',
           'matplotlib==2.2.2',
-          #'Werkzeug==0.14.1',
-          #'tensorflow==1.12.0'
       ],
 )
